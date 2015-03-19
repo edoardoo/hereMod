aec74a2189813bf68e653c1d27564fbe24f24b1b
edoardoo <edoubuntu@gmail.com>
2015-03-20 00:06:16 +0100

    added bookmarklet

---
+++ bookmarklet.js
@@ -0,0 +1,5 @@
+javascript: (function () {     
+	var jsCode = document.createElement('script');     
+	jsCode.setAttribute('src', 'https://rawgit.com/edoardoo/2204168e8f0f46922d54/raw/hereMod.js');                    
+	document.body.appendChild(jsCode);  
+}());

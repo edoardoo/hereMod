73b9bcd9661be702ce31548486975248391a859c
edoardoo <edoubuntu@gmail.com>
2015-04-02 13:52:05 +0200

    changin names

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,5 +1,5 @@
 javascript: (function () {     
 	var jsCode = document.createElement('script');     
-	jsCode.setAttribute('src', 'https://rawgit.com/edoardoo/2204168e8f0f46922d54/raw/hereMod.js');                    
+	jsCode.setAttribute('src', 'https://rawgit.com/edoardoo/2204168e8f0f46922d54/raw/there.js');                    
 	document.body.appendChild(jsCode);  
 }());

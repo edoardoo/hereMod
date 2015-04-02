57f37061838bf857203906cea4334fd08dc02eaa
edoardoo <edoubuntu@gmail.com>
2015-04-02 03:38:35 +0200

    initial page

--- bookmarklet.js
+++
@@ -1,5 +0,0 @@
-javascript: (function () {     
-	var jsCode = document.createElement('script');     
-	jsCode.setAttribute('src', 'https://rawgit.com/edoardoo/2204168e8f0f46922d54/raw/hereMod.js');                    
-	document.body.appendChild(jsCode);  
-}());

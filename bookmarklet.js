be9b34376ca88b530571e9e4c9ac3f5dd9d2d9e4
edoardoo <edoubuntu@gmail.com>
2015-04-09 17:06:31 +0200

    modified bookmarklet to use v0.1 tagged version

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,5 +1,5 @@
 javascript: (function () {     
 	var jsCode = document.createElement('script');     
-	jsCode.setAttribute('src', 'https://cdn.rawgit.com/edoardoo/there/master/there.js');                    
+	jsCode.setAttribute('src', 'https://cdn.rawgit.com/edoardoo/there/v0.1/there.js');                    
 	document.body.appendChild(jsCode);  
 }());
